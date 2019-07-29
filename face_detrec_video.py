c64dca2b6d5a66fcdc682b99ed715463281ee986
base0 <vacapp@live.com>
2019-07-29 23:51:37 +0700

    opencv 3 on Raspbian 10

--- face_detrec_video.py
+++ face_detrec_video.py
@@ -21,11 +21,12 @@ Takes one argument:
 '''
 
 import cv2
-import cv2.cv as cv
+#import cv2.cv as cv
 import numpy as np
 import os
 import sys, time
-import requests, facebook
+
+arg_one= '/home/pi/FaceRecognizer/sorted_output_images'
 
 def get_images(path, size):
     '''
@@ -41,7 +42,7 @@ def get_images(path, size):
 
     for subdir in os.listdir(path):
         for image in os.listdir(path+ "/"+ subdir):
-            #print(subdir, images)
+            print(subdir, images)
             img= cv2.imread(path+os.path.sep+subdir+os.path.sep+image, cv2.IMREAD_GRAYSCALE)
             img= cv2.resize(img, size)
 
@@ -61,7 +62,7 @@ def detect_faces(image):
     Takes an image as input and returns an array of bounding box(es).
     '''
     frontal_face= cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    bBoxes= frontal_face.detectMultiScale(image, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
+    bBoxes= frontal_face.detectMultiScale(image, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv2.CASCADE_SCALE_IMAGE)
 
     return bBoxes
 
@@ -70,7 +71,7 @@ def train_model(path):
     Takes path to images and train a face recognition model
     Returns trained model and people
     '''
-    [images, labels, people]= get_images(sys.argv[1], (256, 256))
+    [images, labels, people]= get_images(arg_one, (256, 256))
     #print([images, labels])
 
     labels= np.asarray(labels, dtype= np.int32)
@@ -78,7 +79,7 @@ def train_model(path):
     # initializing eigen_model and training
     print("Initializing eigen FaceRecognizer and training...")
     sttime= time.clock()
-    eigen_model= cv2.createEigenFaceRecognizer()
+    eigen_model= cv2.face.createEigenFaceRecognizer()
     eigen_model.train(images, labels)
     print("\tSuccessfully completed training in "+ str(time.clock()- sttime)+ " Secs!")
 
@@ -99,43 +100,11 @@ def majority(mylist):
 
     return ans
 
-def post_on_facebook(intruder, counter, picture_name):
-    '''
-    Takes name of intruder and posts on your facebok timeline.
-    You need to get access_token from facebook GraphAPI and paste it below.
-    '''
-    # has a life time of 1 hr. So, no use even if you steal this 😜
-    token= "CAACEdEose0cBAPr3Hjm3zudDaDg0CHZBbWj9TBKyBJH6NSXkNYT9nCqvMnp5rdjjBStMkt8aiicc22tyZBs5wb8g4jZCg2wfoBQUc8C7p38VoZBQWRgbZAZCQ8MDjeBFZBxvs5Ex0X0QhKor3ZAJMZBvjWXFx0Rdd6lDdhuwvfZCeaKRbM4kTyXbZCwHpXmsj6kX4bJ1ZA5JDMZBdLXJYeV9Bl1zM"
-    url= "https://graph.facebook.com/me/feed"
-
-    graph= facebook.GraphAPI(access_token= token)
-
-    my_message1= "Surya is not in his room at present and '"+ intruder+ "' entered into his room without permission."
-    my_message2= "PS: This is automatically posted by 'intruder alert system' built by Surya!\n"
-    final_message= my_message1+"\n\n"+my_message2+ "\n"+ str(counter)
-
-    #post on facebook using requests.
-    # params= {"access_token": token, "message": final_message}
-    # posted= requests.post(url, params)
-
-    # if str(posted)== "<Response [200]>":
-    #     print("\tSuccessfully posted on your timeline.")
-    # else:
-    #     print("\tPlease check your token and its permissions.")
-    #     print("\tYou cannot post same message more than once in a single POST request.")
-
-    #post on facebook using python GraphAPI
-    graph.put_photo(image= open(picture_name), message= final_message)
-
-
 
 if __name__== "__main__":
-    if len(sys.argv)!= 2:
-        print("Wrong number of arguments! See the usage.\n")
-        print("Usage: face_detrec_video.py <full/path/to/root/images/folder>")
-        sys.exit()
 
-    arg_one= sys.argv[1]
+
+
     eigen_model, people= train_model(arg_one)
 
     #starts recording video from camera and detects & predict subjects
@@ -160,7 +129,7 @@ if __name__== "__main__":
             crop_gray_frame= gray_frame[q:q+s, p:p+r]
             crop_gray_frame= cv2.resize(crop_gray_frame, (256, 256))
 
-            [predicted_label, predicted_conf]= eigen_model.predict(np.asarray(crop_gray_frame))
+            [predicted_label, predicted_conf] = eigen_model.predict(np.asarray(crop_gray_frame))
             last_20.append(predicted_label)
             last_20= last_20[1:]
 
@@ -170,32 +139,21 @@ if __name__== "__main__":
                 But, posting on facebook always use max_label as a parameter.
             '''
 
+            box_text= format("Subject: "+ people[predicted_label])
             cv2.putText(frame, box_text, (p-20, q-5), cv2.FONT_HERSHEY_PLAIN, 1.3, (25,0,225), 2)
 
+            '''
             if counter%10== 0:
                 max_label= majority(last_20)
                 #box_text= format("Subject: "+ people[max_label])
                 box_text= format("Subject: "+ people[predicted_label])
-
-                if counter> 20:
-                    print("Will post on facebook timeline if this counter reaches to 5: "+ str(len(final_5)+ 1))
-                    final_5.append(max_label)       #it always takes max_label into consideration
-                    if len(final_5)== 5:
-                        final_label= majority(final_5)
-                        print("Intruder is "+ people[final_label])
-                        print("Posting on your facebook timeline...")
-                        picture_name= "frame.jpg"
-                        cv2.imwrite(picture_name, frame)
-                        post_on_facebook(people[final_label], counter, picture_name)
-                        final_5= []
-
-
+            '''
 
 
         cv2.imshow("Video Window", frame)
         counter+= 1
 
-        if (cv2.waitKey(5) & 0xFF== 27):
+        if (cv2.waitKey(5) & 0xFF == ord('q')):
             break
 
     cv2.destroyAllWindows()
